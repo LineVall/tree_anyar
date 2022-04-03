d9daab749c42633f3c4e90af4e3fc0d9bd793ca7
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    sweet: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- device.mk
+++ device.mk
@@ -88,6 +88,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
