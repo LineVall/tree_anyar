5c7067f3dd67f66fceeb71b29048ce01639d8927
LuK1337 <priv.luk@gmail.com>
2025-04-05 22:34:15 +0200

    sweet: Add libwfdservice_shim for WFD
    e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I60691f2bab3f52fd9ade36ed6fcf6a2ef13a18c9
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -45,42 +45,44 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/etc/init/wfdservice.rc': blob_fixup()
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
-     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+    'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace(' +seclabel u:r:mi_thermald:s0\n', ''),
-     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
-     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
+    ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-     ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
-         .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
-         .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
-         .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
-     ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
-         .clear_symbol_version('AHardwareBuffer_allocate')
-         .clear_symbol_version('AHardwareBuffer_describe')
-         .clear_symbol_version('AHardwareBuffer_lock')
-         .clear_symbol_version('AHardwareBuffer_release')
-         .clear_symbol_version('AHardwareBuffer_unlock'),
-     ('vendor/lib64/libarcsoft_hta.so', 'vendor/lib64/libarcsoft_super_night_raw.so', 'vendor/lib64/libhvx_interface.so', 'vendor/lib64/libmialgo_rfs.so'): blob_fixup()
-         .clear_symbol_version('remote_handle_close')
-         .clear_symbol_version('remote_handle_invoke')
-         .clear_symbol_version('remote_handle_open')
-         .clear_symbol_version('remote_handle64_close')
-         .clear_symbol_version('remote_handle64_invoke')
-         .clear_symbol_version('remote_handle64_open')
-         .clear_symbol_version('remote_register_buf_attr')
-         .clear_symbol_version('remote_register_buf')
-         .clear_symbol_version('rpcmem_alloc')
-         .clear_symbol_version('rpcmem_free')
-         .clear_symbol_version('rpcmem_to_fd'),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
+    ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    ('vendor/lib64/libarcsoft_hta.so', 'vendor/lib64/libarcsoft_super_night_raw.so', 'vendor/lib64/libhvx_interface.so', 'vendor/lib64/libmialgo_rfs.so'): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open')
+        .clear_symbol_version('remote_register_buf_attr')
+        .clear_symbol_version('remote_register_buf')
+        .clear_symbol_version('rpcmem_alloc')
+        .clear_symbol_version('rpcmem_free')
+        .clear_symbol_version('rpcmem_to_fd'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
