dc69d9fca6faa830273d24e899b35fa959bfdcf3
unsatifsed27 <unsatifsederror@gmail.com>
2025-05-14 10:38:29 +0700

    sweet: Remove unwanted GApps Packages Build
    Change-Id: Ieb4282a962ad2210a75062dac613d2dfea5498f3
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

---
+++ removegapps/Android.mk
@@ -0,0 +1,60 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := removegapps
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    Aperture \
+    AmbientStreaming \
+    arcore \
+    Camera2 \
+    CarrierLocation \
+    CarrierMetrics \
+    Chrome64 \
+    Chrome \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DiagnosticsToolPrebuilt \
+    DMService \
+    Drive \
+    FilesGoogle \
+    Gmail2 \
+    Gmail \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    Photos \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    USCCDM \
+    Velvet \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
